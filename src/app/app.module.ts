c30be736575ddab003df42aff78bc6bed053eb68
tremacoda <mario.bergognoni@gmail.com>
2020-02-11 18:04:59 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
+import { products } from '../products';
 
 @NgModule({
   imports: [
